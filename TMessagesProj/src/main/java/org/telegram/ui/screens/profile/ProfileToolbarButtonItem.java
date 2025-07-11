375f3733ceaa742a4711d84beca4489eb82cf9f2
Omid Heshmatinia <omidheshmatinia@gmail.com>
2025-07-11 17:11:40 +0800

    feat: add new buttons and icons for toolbar

--- TMessagesProj/src/main/java/org/telegram/ui/screens/profile/ProfileToolbarButtonItem.java
+++ TMessagesProj/src/main/java/org/telegram/ui/screens/profile/ProfileToolbarButtonItem.java
@@ -9,6 +9,17 @@ public enum ProfileToolbarButtonItem {
     Message(R.drawable.filled_message, R.string.Message),
     Call(R.drawable.filled_call, R.string.Call),
     Video(R.drawable.filled_video, R.string.GroupCallCreateVideo),
+    Leave(R.drawable.msg_leave, R.string.VoipGroupLeave),
+    VoiceChat(R.drawable.live_stream, R.string.StartVoipChatTitle),
+    Join(R.drawable.join, R.string.VoipChatJoin),
+    Report(R.drawable.report, R.string.ReportBot),
+    Share(R.drawable.filled_share, R.string.VoipChatShare),
+    Discuss(R.drawable.filled_message, R.string.Discuss),
+    Stop(R.drawable.block, R.string.Stop),
+    Gift(R.drawable.filled_gift, R.string.ActionStarGift),
+    AddStory(R.drawable.story, R.string.AddStory),
+    LiveStream(R.drawable.live_stream, R.string.VoipChannelVoiceChat),
+    Mute(R.drawable.filled_mute, R.string.VoipGroupLeave),
     UnMute(R.drawable.filled_unmute, R.string.ChatsUnmute);
 
     public final @DrawableRes  int icon;
